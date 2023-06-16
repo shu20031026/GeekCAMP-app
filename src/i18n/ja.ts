df55b98e3c8cc3d61ab91d9b3df5319133685f4d
shu20031026 <shu20031026@gmail.com>
2023-06-16 23:10:29 +0900

    text一覧を作成

--- src/i18n/ja.ts
+++ src/i18n/ja.ts
@@ -1,5 +1,20 @@
 const ja = {
-  siteName: 'next-template',
+  appName: 'ChatMasters',
+  mode: {
+    evaluate: {
+      name: 'メッセージ判別モード',
+      description: '入力されたメッセージから、会話のフォーマル度の目安を0%から100%で判別します',
+    },
+    translate: {
+      name: 'メッセージ変換モード',
+      description: '入力されたメッセージを指定したフォーマル度に変換します',
+    },
+  },
+  button: {
+    changeMode: 'モードを変更する',
+    evaluate: '評価する',
+    translate: '変換する',
+  },
 } as const;
 
 export default ja;
